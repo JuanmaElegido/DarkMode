bccd4e4207f6bae89fb91bc5e8e4cbe5794e91b5
JuanmaElegido <88286028+JuanmaElegido@users.noreply.github.com>
2022-10-04 00:27:01 +0200

    Persistence

--- js/darkMode.js
+++ js/darkMode.js
@@ -13,6 +13,8 @@ function printFirstButton() {
     "</div>";
 
   document.getElementById("darkModeDom").innerHTML = printDom;
+
+  initValues();
 }
 
 function darkMode(idButton, increase) {
@@ -22,49 +24,81 @@ function darkMode(idButton, increase) {
     domButton.classList.toggle("btn-light");
 
     if (domButton.innerHTML === "Light") {
-      domButton.innerHTML = "Dark";
-      document.body.style.backgroundColor = "#222";
-      document.body.style.color = "#fff";
+      darkColor(domButton);
     } else {
-      domButton.innerHTML = "Light";
-      document.body.style.backgroundColor = "#fff";
-      document.body.style.color = "#222";
+      lightColor(domButton);
     }
   } else {
-    let nColor = 0;
+    let numberColor = 0;
     let bColor = document.body.style.backgroundColor;
     bColor = bColor.slice(4, 7).replace(",", "");
 
-    nOriginalColor = bColor / 17;
-    nColor = bColor / 17 - increase;
-    bColor = nColor.toString();
-    bColor = "#" + bColor + bColor + bColor;
+    let nOriginalColor = bColor / 17;
+    numberColor = bColor / 17 - increase;
+    chooseColor(numberColor, nOriginalColor, domButton);
+  }
+}
 
-    if (bColor == "#141414") {
-      document.body.style.backgroundColor = "#999";
-    } else if (bColor == "#101010") {
-      document.body.style.backgroundColor = "#fff";
-    } else if (bColor != "#161616") {
-      document.body.style.backgroundColor = bColor;
-    }
+function chooseColor(numberColor, nOriginalColor, domButton) {
+  localStorage.setItem("numberColor", numberColor);
+  bColor = numberColor.toString();
+  bColor3 = numberColor.toString();
+  bColor = "#" + bColor + bColor + bColor3;
 
-    if (nColor < 9) {
-      document.body.style.color = "#fff";
+  if (bColor == "#141414") {
+    document.body.style.backgroundColor = "#999";
+  } else if (bColor == "#101010") {
+    document.body.style.backgroundColor = "#fff";
+  } else if (bColor != "#161616") {
+    document.body.style.backgroundColor = bColor;
+  }
+
+  if (numberColor < 9) {
+    document.body.style.color = "#fff";
+  } else {
+    document.body.style.color = "#222";
+  }
+
+  if (
+    (numberColor == 5 && nOriginalColor == 6) ||
+    (numberColor == 6 && nOriginalColor == 5)
+  ) {
+    domButton.classList.toggle("btn-dark");
+    domButton.classList.toggle("btn-light");
+    if (domButton.innerHTML === "Light") {
+      domButton.innerHTML = "Dark";
     } else {
-      document.body.style.color = "#222";
-    }
-
-    if (
-      (nColor == 5 && nOriginalColor == 6) ||
-      (nColor == 6 && nOriginalColor == 5)
-    ) {
-      domButton.classList.toggle("btn-dark");
-      domButton.classList.toggle("btn-light");
-      if (domButton.innerHTML === "Light") {
-        domButton.innerHTML = "Dark";
-      } else {
-        domButton.innerHTML = "Light";
-      }
+      domButton.innerHTML = "Light";
     }
   }
+}
+
+function initValues() {
+  debugger;
+  let domButton = document.getElementById("button1");
+  var isNumberColor = localStorage.getItem("numberColor");
+  if (isNumberColor) {
+    chooseColor(isNumberColor, isNumberColor, domButton);
+    if (isNumberColor <= 5) {
+      domButton.classList.toggle("btn-dark");
+      domButton.classList.toggle("btn-light");
+      domButton.innerHTML = "Dark";
+    } else {
+      domButton.innerHTML = "Light";
+    }
+  }
+}
+
+function darkColor(domButton) {
+  domButton.innerHTML = "Dark";
+  localStorage.setItem("numberColor", 2);
+  document.body.style.backgroundColor = "#222";
+  document.body.style.color = "#fff";
+}
+
+function lightColor(domButton) {
+  domButton.innerHTML = "Light";
+  localStorage.setItem("numberColor", 10);
+  document.body.style.backgroundColor = "#fff";
+  document.body.style.color = "#222";
 }
